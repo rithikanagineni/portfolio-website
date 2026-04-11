bee702c63493b9e5f8616bd9c42edb2276784716
Rithika Nagineni <rithikanagineni@gmail.com>
2026-04-11 19:05:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function showMessage() {
   alert("Welcome to Rithika's Portfolio 🚀");
 }
+
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+
+  if (name === "" || email === "") {
+    alert("Please fill all fields");
+    return false;
+  }
+
+  alert("Form submitted successfully!");
+  return true;
+}
+
+console.log("Portfolio loaded successfully");
